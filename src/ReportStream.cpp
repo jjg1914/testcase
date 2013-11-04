771b7fdf0f55088a0e4d157b272999df4a6e2125
John J. Glynn IV <jjg1914@rit.edu>
2013-11-03 20:17:16 -0500

    Added ReportStream for IPC.

---
+++ src/ReportStream.cpp
@@ -0,0 +1,37 @@
+#include "unistd.h"
+
+#include "ReportStream.h"
+
+using namespace std;
+using namespace __gnu_cxx;
+
+ReportStream::ReportStream()
+  : p(),
+    inbuff(p.in, ios::in),
+    outbuff(p.out, ios::out),
+    in(&inbuff),
+    out(&outbuff)
+{}
+
+ReportStream::_pipe::_pipe()
+  : in(fileds[0]),
+    out(fileds[1])
+{
+  pipe(fileds);
+}
+
+ReportStream &ReportStream::operator<<(const TestInfo &info)
+{
+  out << (string) info << endl;
+  return *this;
+}
+
+ReportStream &ReportStream::operator>>(TestInfo &info)
+{
+  string buf, line;
+  while (getline(in, line), !in.eof() && line != "") {
+    buf += line + "\n";
+  }
+  info = TestInfo(buf);
+  return *this;
+}
